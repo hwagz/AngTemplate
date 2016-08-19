7251b6802bcaffe007e410578fc7722ece6c9baf
hwagz <hunt.wagner@gmail.com>
2016-08-18 22:51:34 -0400

    Adjusted js files to hosted from link for offline editing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 angular
-  .module('APPNAME',[])
-  .controller("APPNAMECtrl", ['$scope','$filter','$compile',function($scope, $filter, $compile) {
+  .module('APPNAME',['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/url1',{
+      templateUrl: "html/url1.html",
+      controller: "url1Ctrl"
+    })
+    .when('/url2',{
+      templateUrl: "html/url2.html",
+      controller: "url2Ctrl"
+    })
+    .when('/url3',{
+      templateUrl: "html/url3.html",
+      controller: "url3Ctrl"
+    })
+    .otherwise({
+      templateUrl: "html/main.html",
+      controller: "mainCtrl"
+    });
+  })
+  .controller('APPNAMECtrl', ['$scope','$filter',function($scope, $filter) {
 
   }])
   .filter('capitalize', function() {
